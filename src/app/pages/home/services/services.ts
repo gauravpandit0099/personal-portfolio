5541edfb63c2a0a01bf6351a527f4967ae6ab8c2
Gaurav Pandit <gaurav.pandit0399@gmail.com>
2026-09-02 18:35:01 +0530

    project structure

---
+++ src/app/pages/home/services/services.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-services',
+  styleUrl: './services.css',
+  templateUrl: './services.html',
+})
+export class Services {}
