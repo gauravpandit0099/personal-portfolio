9fa59c12520d6234136514e14222e8b4c52b18ca
Gaurav Pandit <gaurav.pandit0399@gmail.com>
2026-09-08 21:38:47 +0530

    contact section done

--- src/app/pages/home/contact/contact.ts
+++ src/app/pages/home/contact/contact.ts
@@ -1,9 +1,44 @@
 import { Component } from '@angular/core';
 
 @Component({
-  imports: [],
   selector: 'app-contact',
-  styleUrl: './contact.css',
+  standalone: true,
+  imports: [],
   templateUrl: './contact.html',
+  styleUrl: './contact.css'
 })
-export class Contact {}
+export class Contact {
+  contactDetails = [
+    {
+      label: 'Email',
+      value: 'gaurav.pandit0399@gmail.com',
+      href: 'mailto:gaurav.pandit0399@gmail.com'
+    },
+    {
+      label: 'LinkedIn',
+      value: 'Connect with me',
+      href: 'https://www.linkedin.com/in/gaurav-pandit-7684bb290/'
+    },
+    {
+      label: 'Phone',
+      value: '+91 9575769452',
+      href: 'tel:+919575769452'
+    },
+    {
+      label: 'Location',
+      value: 'Indore, India',
+      href: '#'
+    }
+  ];
+
+  socials = [
+    {
+      name: 'LinkedIn',
+      href: 'https://www.linkedin.com/in/gaurav-pandit-7684bb290/'
+    },
+    // {
+    //   name: 'GitHub',
+    //   href: '#'
+    // }
+  ];
+}
