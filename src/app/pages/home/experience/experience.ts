b1e6f98c56d4776d90033cf1fb824f0dc9e088a3
Gaurav Pandit <gaurav.pandit0399@gmail.com>
2026-09-08 22:05:56 +0530

    redesigend skills and experience

--- src/app/pages/home/experience/experience.ts
+++ src/app/pages/home/experience/experience.ts
@@ -17,7 +17,7 @@ export class Experience {
       period: 'May 2024 — Present',
       type: 'Full Time',
       description:
-        'Developing scalable web applications and business solutions using Angular and Laravel. Working across frontend interfaces, backend APIs, database architecture and application workflows.',
+        'Developing and maintaining enterprise web applications using Laravel and React.js, working across frontend interfaces, backend APIs, MySQL databases and application workflows. Building reusable components, implementing business logic, integrating APIs and contributing to scalable modules and role-based application features.',
       technologies: [
         { name: 'Laravel', color: 'text-red-400' },
         { name: 'React.js', color: 'text-sky-400' },
@@ -26,7 +26,6 @@ export class Experience {
         { name: 'Microservices', color: 'text-blue-400' },
         { name: 'Git', color: 'text-orange-400' },
         { name: 'Postman', color: 'text-yellow-300' },
-        { name: 'Jira', color: 'text-indigo-400' }
       ]
     },
     {
@@ -37,15 +36,16 @@ export class Experience {
       period: 'Sep 2022 — March 2024',
       type: 'Full Time',
       description:
-        'Built responsive and interactive Angular applications while integrating REST APIs and developing reusable components for scalable frontend systems.',
+        'Developed responsive web and mobile applications using Angular and React Native, with a focus on reusable components, API integration and interactive user interfaces. Worked with TypeScript and JavaScript to implement application features, connect frontend systems with REST APIs and support scalable application development.',
       technologies: [
         { name: 'React Native', color: 'text-cyan-400' },
         { name: 'Angular', color: 'text-violet-400' },
         { name: 'TypeScript', color: 'text-blue-400' },
         { name: 'JavaScript', color: 'text-yellow-400' },
         { name: 'Git', color: 'text-orange-400' },
-        { name: 'Postman', color: 'text-red-400' }
+        { name: 'Postman', color: 'text-red-400' },
+        { name: 'Jira', color: 'text-indigo-400' }
       ]
     }
-  ];
+  ];  
 }
